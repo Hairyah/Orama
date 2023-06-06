9177d796416f0dd1dd6e8f3f35802e63ad70f8d4
Hairyah <alexis.lejeune76@hotmail.fr>
2023-06-06 13:54:55 +0200

    Projet finalisé ?

--- Assets/TheEndScript.cs
+++ Assets/TheEndScript.cs
@@ -6,11 +6,22 @@ public class TheEndScript : MonoBehaviour
 {
     public GameObject endGame;
 
+    public GameObject texteFIN;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Joueur")
         {
             endGame.SetActive(true);
+
+            StartCoroutine(texte());
         }
+    }
+
+    IEnumerator texte()
+    {
+        yield return new WaitForSeconds(5f);
+
+        texteFIN.SetActive(true);
     }
 }
